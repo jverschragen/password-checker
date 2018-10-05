f8c41578fb7fa0afe24b0d093bd518d90b627196
Jelle Verschragen <jelle@hotmail.com>
2018-10-05 14:42:17 +0200

    Added optimizer for css and javascript. Also set up building the application

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,75 @@
 var gulp = require('gulp');
-
-// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify-es').default;
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-var browserSync = require('browser-sync').create();
 
+// Development Tasks
+// -----------------
+
+// Start browserSync server
 gulp.task('browserSync', function() {
-    browserSync.init({
+    browserSync({
         server: {
             baseDir: 'app'
-        },
+        }
     })
 })
 
 gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+        .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+        .pipe(browserSync.reload({ // Reloading with Browser Sync
             stream: true
-        }))
-});
+        }));
+})
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
+// Watchers
+gulp.task('watch', function() {
     gulp.watch('app/scss/**/*.scss', ['sass']);
-    // Reloads the browser whenever HTML or JS files change
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks
+// ------------------
+
+// Optimizing CSS and JavaScript
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JS file
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
 });
+
+// Cleaning
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync'], 'watch',
+        callback
+    )
+})
+
+gulp.task('build', function(callback) {
+    runSequence(
+        'clean:dist',
+        'sass',
+        ['useref'],
+        callback
+    )
+})
